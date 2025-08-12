b5643a05c83c5c3c236ed1131c80c624c91ddad6
backend <backenddeveloper.chs@gmail.com>
2025-08-12 14:08:35 +0530

    learn conditions

---
+++ Learning_c_sharp/LearnConditions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LearnConditions
+{
+    class LearnConditions
+    {
+        static void Main()
+        {
+            Console.WriteLine("Learn Conditions in C#");
+
+            bool val = true;
+            int a = 10;
+            int b = 20;
+
+
+            if (a < b)
+            {
+                Console.WriteLine("The value is true");
+            }
+            else
+            {
+                Console.WriteLine("The value is false");
+            }
+        }
+    }
+}
+
